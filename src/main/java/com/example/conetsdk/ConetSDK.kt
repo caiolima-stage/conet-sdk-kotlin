f9ce70c071b19fc51163cbffdc468490faa4e016
caiolima-stage <caio.lima@stage.consulting>
2024-10-14 13:34:04 -0300

    chore: sdk wallet local storage

--- src/main/java/com/example/conetsdk/ConetSDK.kt
+++ src/main/java/com/example/conetsdk/ConetSDK.kt
@@ -1,13 +1,48 @@
-package com.example.conetsdk
 
+import android.content.Context
+import android.content.SharedPreferences
 import org.web3j.crypto.Bip32ECKeyPair
 import org.web3j.crypto.Credentials
 import org.web3j.crypto.MnemonicUtils
-import org.web3j.crypto.WalletUtils
 import org.web3j.utils.Numeric
 import java.security.SecureRandom
 
 object ConetSDK {
+    private lateinit var preferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+
+    fun initialize(context: Context) {
+        preferences = context.getSharedPreferences("wallet", Context.MODE_PRIVATE)
+        editor = preferences.edit()
+    }
+
+    fun getWallet(): Map<String, String>? {
+        val keys = listOf("mnemonic", "private-key", "address")
+        val walletData = mutableMapOf<String, String>()
+
+        for (key in keys) {
+            val value = preferences.getString(key, null)
+            if (value != null) {
+                walletData[key] = value
+            }
+        }
+
+        return if (walletData.size == keys.size) {
+            walletData
+        } else {
+            null
+        }
+    }
+
+    fun saveWallet(wallet: Map<String, String>) {
+        val editor = preferences.edit()
+        for ((key, value) in wallet) {
+            editor.putString(key, value)
+        }
+
+        editor.apply()
+    }
+
     fun generateMnemonic(): String {
         val initialEntropy = ByteArray(16)
         SecureRandom().nextBytes(initialEntropy)
@@ -29,15 +64,27 @@ object ConetSDK {
 
         return mapOf(
             "mnemonic" to mnemonic,
-            "privateKey" to privateKey,
+            "private-key" to privateKey,
             "address" to walletAddress
         )
     }
 
-    fun createWallet(): Map<String, String?> {
-        val mnemonic = generateMnemonic()
-        val walletInfo = createWalletFromMnemonic(mnemonic)
+    fun createWalletIfNotExists(): Map<String, String?> {
+        val existingWallet = getWallet()
 
-        return walletInfo
+        if (existingWallet != null) {
+            println("WALLET EXISTS: " + existingWallet["private-key"])
+
+            return existingWallet
+        } else {
+            println("NO WALLET")
+
+            val mnemonic = generateMnemonic()
+            val walletInfo = createWalletFromMnemonic(mnemonic)
+
+            saveWallet(walletInfo)
+
+            return walletInfo
+        }
     }
 }
